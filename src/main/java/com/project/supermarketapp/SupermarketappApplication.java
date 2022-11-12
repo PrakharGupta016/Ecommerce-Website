495f55d5abe9678ec4574f0a2d82384042f91aec
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2022-11-12 14:35:41 +0530

    project setup

---
+++ src/main/java/com/project/supermarketapp/SupermarketappApplication.java
@@ -0,0 +1,13 @@
+package com.project.supermarketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketappApplication.class, args);
+	}
+
+}
