d65826ec9833c7867fa8372f4b6587986ba8cda3
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2022-11-21 18:45:19 +0530

    error fix

--- src/main/java/com/project/supermarketapp/controllers/UserController.java
+++ src/main/java/com/project/supermarketapp/controllers/UserController.java
@@ -8,8 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    @GetMapping("/")
-    public User getUsers(){
-    }
 }
 
--- src/main/java/com/project/supermarketapp/respository/UserRepo.java
+++ src/main/java/com/project/supermarketapp/respository/UserRepo.java
@@ -1,5 +1,8 @@
 package com.project.supermarketapp.respository;
 
-public interface UserRepo {
+import com.project.supermarketapp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo  extends JpaRepository<User,Integer> {
 
 }
