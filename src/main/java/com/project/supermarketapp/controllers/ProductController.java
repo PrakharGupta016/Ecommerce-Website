41f3a3bac5bb56fd271f8f8f56883bd6d768720b
uxygrs <guptaudit138@gmail.com>
2022-12-05 02:05:11 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/project/supermarketapp/config/SecurityConfig.java
    #	src/main/java/com/project/supermarketapp/entities/User.java

--- src/main/java/com/project/supermarketapp/controllers/ProductController.java
+++ src/main/java/com/project/supermarketapp/controllers/ProductController.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("api/product")
 public class ProductController {
     @Autowired
     ProductService productService;
@@ -28,7 +28,7 @@ public class ProductController {
     @Autowired
     CategoryRepo categoryRepo;
 
-    @PostMapping("/add")
+    @PostMapping("/")
     public ResponseEntity<ApiResponse> createProduct(@RequestBody ProductDto productDto) {
         Optional<Category> optionalCategory = categoryRepo.findById(productDto.getCategoryId());
         if (!optionalCategory.isPresent()) {
